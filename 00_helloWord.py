5fb8aa0e47ad419ea0d3b823804ddf3db36311b2
Brenda Noelia Veramendi <brendaveramendi@gmail.com>
2024-01-08 08:27:27 -0300

    first commit

---
+++ 00_helloWord.py
@@ -0,0 +1,8 @@
+print('Hola Mundo')
+print(type('Hola Python')) #Tipo cadena
+print(type("Soy un dato str"))  # Tipo 'str'
+print(type(5))  # Tipo 'int'
+print(type(1.5))  # Tipo 'float'
+print(type(3 + 1j))  # Tipo 'complex'
+print(type(True))  # Tipo 'bool'
+print(type(print("Mi cadena de texto")))  # Tipo 'NoneType'
---
+++ 01_variables.py
@@ -0,0 +1,28 @@
+#Variables
+
+#Se recomienda separar por guion bajo
+my_variable = "My string variable"
+print(my_variable)
+
+'''
+Este es un comentario
+'''
+
+#Funciones del sistema
+print(len(my_variable))
+
+#Asignacion
+
+my_bool_variable = False
+
+#Variables en una sola línea
+
+name, surname, alias = 'Brenda', 'Veramendi', 'brendaveramendi'
+print('Me llamo: ', name, surname, 'Alias: ', alias)
+
+#Ingresar datos desde consola
+
+first_name = input('What is your name: ')
+last_name = input('What is your last name: ')
+
+print(first_name, last_name)
