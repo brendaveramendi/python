0fd31c5cd940acc4cb207cf2d8d458d7e4f9753e
Brenda Noelia Veramendi <brendaveramendi@gmail.com>
2024-01-11 08:16:44 -0300

    modules

---
+++ 13_modules.py
@@ -0,0 +1,9 @@
+#Modules
+import module
+module.sum(5,3)
+module.printValue('Hola Python')
+
+from module import sum, printValue
+sum(5,3)
+printValue('Hola Python')
+
---
+++ module.py
@@ -0,0 +1,5 @@
+def sum(number_one, number_two):
+  print(number_one + number_two)
+  
+def printValue(value):
+  print(value)
