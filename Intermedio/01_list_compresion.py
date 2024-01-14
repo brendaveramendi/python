2de881092b8dbace6c1c8e15f2cf1882f06504ce
Brenda Noelia Veramendi <brendaveramendi@gmail.com>
2024-01-13 21:21:30 -0300

    challenge-01

---
+++ Intermedio/01_list_compresion.py
@@ -0,0 +1,11 @@
+#lista compresion
+
+my_original_list = [0,1,2,3,4,5,6,7]
+
+print(my_original_list)
+
+my_list = [i for i in range(8)]
+print(my_list)
+
+my_list = [i * 2  for i in range(8)]
+print(my_list)
---
+++ Intermedio/02_challenges.py
@@ -0,0 +1,85 @@
+"""
+El famosos Fizz buzz
+Escribe un programa que muestre por consola
+con un print los numeros del 1 al 100 (con salto de linea cada numero)
+Multiplos de 3 por la pañabra fizz
+Multiplos de 5 por la pañabra "buzz"
+Multiplos de 3 y 5 a la vez por la
+palabra "fizzbuzz"  
+"""
+def fizzbuzz():
+  lista_buf = []
+  for index in range(1,101):
+   if index % 3 == 0 and index % 5 ==0:
+    print (index)
+   elif index % 3:
+    print (index)
+   elif index % 5:
+    print (index)
+   else:
+    print(lista_buf)
+  
+     
+fizzbuzz() 
+
+
+"""
+¿ES UN ANAGRAMA?
+Escribe una función que reciba dos palabras (String) y retorne
+verdadero o falso (Bool) según sean o no anagramas.
+- Un Anagrama consiste en formar una palabra reordenando TODAS
+  las letras de otra palabra inicial.
+- NO hace falta comprobar que ambas palabras existan.
+- Dos palabras exactamente iguales no son anagrama.
+"""
+def is_anagram(word_one, word_two):
+  
+  return sorted(word_one.lower()) == sorted(word_two.lower())
+
+
+print(is_anagram("amor", "roma"))
+
+"""
+LA SUCESIÓN DE FIBONACCI
+Escribe un programa que imprima los 50 primeros números de la sucesión
+de Fibonacci empezando en 0.
+- La serie Fibonacci se compone por una sucesión de números en
+  la que el siguiente siempre es la suma de los dos anteriores.
+  0, 1, 1, 2, 3, 5, 8, 13...
+"""
+
+def sucesión_fibonacci():
+  number_one = 0
+  number_two = 1
+ 
+  for index in range(1,51):
+    print(number_one)
+    fibo = number_one + number_two
+    number_one = number_two
+    number_two = fibo  
+    
+sucesión_fibonacci()
+
+"""
+¿ES UN NÚMERO PRIMO?
+Escribe un programa que se encargue de comprobar si un número es o no primo.
+Hecho esto, imprime los números primos entre 1 y 100.
+"""
+print("----------------------")
+
+def primo():
+  for index in range(1,101):
+    flag = True
+    for i in range(2,index//2+1):
+      if(index%i==0):
+        flag = False
+        break
+    if flag:
+     print(index)
+       
+    
+
+print("*********************")        
+primo()
+   
+print(15//2)
