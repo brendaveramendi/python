6292b294d556b5eed9a48053a961814cc2682458
brendaveramendi <brendaveramendi@gmail.com>
2024-01-23 12:40:50 -0300

    Filter

--- Backend/FastAPI/user.py
+++ Backend/FastAPI/user.py
@@ -1,8 +1,28 @@
+from typing import Union
 from fastapi import FastAPI
 from pydantic import BaseModel
 
 app = FastAPI()
 
-@app.get("/users")
-async def users():
+
+#Entidad user
+#Base model me da la capaciadad
+#De crear una entidad
+class User(BaseModel):
+  id: int
+  name: str
+  apellido: str
+
+users_list = [User(id=1, name="Pamela",apellido="Veramendi"),User(id=2, name="Rebeca",apellido="Veramendi"),User(id=3,name="Brenda",apellido="Veramendi")]
+
+
+@app.get("/usersjson")
+async def usersjson():
   return [{"name":"Brenda","Apellido":"Veramendi"},{"name":"Rebeca","Apellido":"Veramendi"}]
+
+@app.get("/user/{id}")
+async def user(id:int):
+  #Filter me devuelve un objeto de tipo iterador
+  users = filter(lambda user: user.id == id,users_list)
+  print(users)
+  return list(users)
