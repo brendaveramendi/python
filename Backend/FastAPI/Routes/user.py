84c7d43eebdbf15bcb997872d303bf8715240889
Brenda Noelia Veramendi <brendaveramendi@gmail.com>
2024-01-29 09:36:25 -0300

    static object

---
+++ Backend/FastAPI/Routes/user.py
@@ -0,0 +1,64 @@
+from typing import Union
+from fastapi import APIRouter, HTTPException
+from pydantic import BaseModel
+
+router = APIRouter()
+
+
+#Entidad user
+#Base model me da la capaciadad
+#De crear una entidad
+class User(BaseModel):
+  id: int
+  name: str
+  apellido: str
+
+users_list = [User(id=1, name="Pamela",apellido="Veramendi"),User(id=2, name="Rebeca",apellido="Veramendi"),User(id=3,name="Brenda",apellido="Veramendi")]
+
+
+@router.get("/users")
+async def usersjson():
+  return users_list
+
+@router.get("/user/{id}") #Path
+async def user(id:int):
+  #Filter me devuelve un objeto de tipo iterador
+  users = filter(lambda user: user.id == id,users_list)
+  print(users)
+  return list(users)[0]
+
+@router.get("/user/") #Query
+async def user(id:int):
+  return search_user(id)
+
+def search_user(id: int):
+  users = filter(lambda user: user.id == id, users_list)
+  try:
+    return list(users)[0]
+  except:
+    return{"erro":"Usuario excistente"}
+#POST
+@router.post("/user/",response_model=User,status_code=201) #Query
+async def user(user: User):
+  if type(search_user(user.id))==User:
+    return HTTPException(204,detail="El usuario ya existe")
+  else:
+    users_list.routerend(user)
+
+#PUT
+#Se usa cuando se actualiza el objeto completo
+#PUTH se usa cuando se actualiza un atributo del objeto
+@router.put("/user/")
+async def user(user:User):
+  for index, saved_user in enumerate(users_list):
+    if saved_user.id == user.id:
+      users_list[index] = user
+      return user
+    else:
+      return {"Error":"No se  ha encontrado el ususario"}
+
+@router.delete("/user/{id}")
+async def user(id:int):
+  for index, saved_user in enumerate(users_list):
+    if saved_user.id == id:
+      del users_list[index]
---
+++ Backend/FastAPI/main.py
@@ -0,0 +1,18 @@
+from typing import Union
+from fastapi import FastAPI
+from Routes import products
+from Routes import user
+from fastapi.staticfiles import StaticFiles
+app = FastAPI()
+
+# Routes
+app.include_router(products.router)
+app.include_router(user.router)
+#1 static como encuentro el recurso
+#2 parametro es el directorio
+#3 Es como se llama el recurso 
+app.mount("/static",StaticFiles(directory="static"),name="static")
+
+@app.get("/")
+async def root():
+  return "Hola FastAPi"
